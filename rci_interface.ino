86b862cc9d3c3a3f2c40d9c6f49ebbf56913dbfb
Quentin Smith <quentin@mit.edu>
2018-05-12 01:00:18 -0400

    It works

--- rci_interface.ino
+++ rci_interface.ino
@@ -1,5 +1,5 @@
-#define WRITE_HIGH D
-#define WRITE_LOW C
+#define WRITE_HIGH C
+#define WRITE_LOW D
 #define READ_HIGH F
 #define READ_LOW B
 
@@ -8,8 +8,10 @@
 #define PORT_WRITE_LOW _CONCATENATE(PORT, WRITE_LOW)
 #define DDR_WRITE_LOW _CONCATENATE(DDR, WRITE_LOW)
 #define PORT_READ_HIGH _CONCATENATE(PORT, READ_HIGH)
+#define PIN_READ_HIGH _CONCATENATE(PIN, READ_HIGH)
 #define DDR_READ_HIGH _CONCATENATE(DDR, READ_HIGH)
 #define PORT_READ_LOW _CONCATENATE(PORT, READ_LOW)
+#define PIN_READ_LOW _CONCATENATE(PIN, READ_LOW)
 #define DDR_READ_LOW _CONCATENATE(DDR, READ_LOW)
 #define _CONCATENATE(reg, letter) _XCONCATENATE(reg, letter)
 #define _XCONCATENATE(reg, letter) (reg ## letter)
@@ -24,8 +26,8 @@
 #define PIN_HCTL_ACTIVE LOW
 #define PIN_HCTL_INACTIVE HIGH
 #define PIN_HREAD PIN_E7
-#define PIN_HREAD_READ LOW
-#define PIN_HREAD_WRITE HIGH
+#define PIN_HREAD_READ HIGH
+#define PIN_HREAD_WRITE LOW
 
 // Milliseconds to wait for HACK
 #define TIMEOUT 100
@@ -56,16 +58,34 @@ void setup() {
 void toggleHctl() {
   hctl ^= 1;
   digitalWrite(PIN_HCTL, hctl);
+  //delay(1);
 }
 
 void doWrite() {
-  long address = strtol(bufPtr, &bufPtr, 16);
+  if (*bufPtr != 'w') {
+    Serial.print("\n! Write request must begin with w, got: ");
+    Serial.print(bufPtr);
+    Serial.print("\n");
+    return;
+  }
+  bufPtr++;
+  Serial.print("\n! Write request received: ");
+  Serial.print(bufPtr);
+  Serial.print("\n");
+  char* endPtr = NULL;
+  long address = strtol(bufPtr, &endPtr, 16);
+  Serial.print("\n! Writing to address ");
+  Serial.print(address, HEX);
+  Serial.print("remaining line: ");
+  Serial.print(endPtr);
+  Serial.print("\n");
   // Toggle HCTL to reset address
   digitalWrite(PIN_HREAD, PIN_HREAD_WRITE);
+  delayMicroseconds(100);
   toggleHctl();
   writeWord(address);
-  char* endPtr = NULL;
   while (true) {
+    bufPtr = endPtr;
     long word = strtol(bufPtr, &endPtr, 16);
     if (endPtr == bufPtr) {
       break;
@@ -77,9 +97,14 @@ void doWrite() {
 }
 
 bool writeWord(long word) {
+  Serial.write("! Write word ");
+  Serial.print(word, HEX);
+  Serial.print("\n");
   unsigned long start = millis();
-  PORT_WRITE_LOW = word&0xFF;
-  PORT_WRITE_HIGH = (word >> 8)&0xFF;
+  PORT_WRITE_LOW = word & 0xFF;
+  PORT_WRITE_HIGH = (word >> 8) & 0xFF;
+  delay(1);
+  delayMicroseconds(100);
   // Assert HREQ
   digitalWrite(PIN_HREQ, PIN_HREQ_ACTIVE);
   // Wait for HACK
@@ -89,6 +114,7 @@ bool writeWord(long word) {
     Serial.print("\n! Write timed out waiting for HACK to become active\n");
     return false;
   }
+  delay(1);
   // Deassert HREQ
   digitalWrite(PIN_HREQ, PIN_HREQ_INACTIVE);
   while (digitalRead(PIN_HACK) != PIN_HACK_INACTIVE && (millis() - start) < TIMEOUT);
@@ -96,6 +122,7 @@ bool writeWord(long word) {
     Serial.print("\n! Write timed out waiting for HACK to become inactive\n");
     return false;
   }
+  delay(1);
   return true;
 }
 
@@ -104,20 +131,25 @@ void loop() {
   while (Serial.available()) {
     *bufPtr = Serial.read();
     bufPtr++;
-    if (*(bufPtr-1) == '\n') {
-      doWrite();
-      memset(buf, 0, sizeof(buf));
+    if (*(bufPtr - 1) == '\n') {
       bufPtr = buf;
+      doWrite();
+      bufPtr = buf;
+      memset(buf, 0, sizeof(buf));
     }
   }
-  delayMicroseconds(500);
+  //delayMicroseconds(500000);
+  delay(500);
   unsigned long start = millis();
   digitalWrite(PIN_HREAD, PIN_HREAD_READ);
+  delayMicroseconds(100);
   // Toggle HCTL to reset address
   toggleHctl();
   for (int i = 0; i < 12; i++) {
     // Assert HREQ
     digitalWrite(PIN_HREQ, PIN_HREQ_ACTIVE);
+    //delay(1);
+    //delayMicroseconds(100);
     // Wait for HACK
     while (digitalRead(PIN_HACK) != PIN_HACK_ACTIVE && (millis() - start) < TIMEOUT);
     if ((millis() - start) >= TIMEOUT) {
@@ -125,8 +157,11 @@ void loop() {
       Serial.print("\n! Read timed out waiting for HACK to become active\n");
       return;
     }
-    Serial.print(PORT_READ_HIGH, HEX);
-    Serial.print(PORT_READ_LOW, HEX);
+    //delay(1);
+    //delayMicroseconds(100);
+    Serial.print(PIN_READ_HIGH, HEX);
+    Serial.print(' ');
+    Serial.print(PIN_READ_LOW, HEX);
     // Deassert HREQ
     digitalWrite(PIN_HREQ, PIN_HREQ_INACTIVE);
     while (digitalRead(PIN_HACK) != PIN_HACK_INACTIVE && (millis() - start) < TIMEOUT);
@@ -135,6 +170,8 @@ void loop() {
       return;
     }
     Serial.print(' ');
+    //delay(1);
+    //delayMicroseconds(100);
   }
   Serial.print('\n');
   Serial.send_now();
