9058c7bef82682b2b386561a400f21dafcc3438a
Quentin Smith <quentin@mit.edu>
2018-05-13 18:19:58 -0400

    More work on serial interface

--- rci/rci.go
+++ rci/rci.go
@@ -3,6 +3,8 @@ package rci
 import (
 	"bufio"
 	"context"
+	"fmt"
+	"log"
 	"strconv"
 	"strings"
 
@@ -10,19 +12,19 @@ import (
 )
 
 type Status struct {
-	Registers [12]uint16
-	Diag      uint16
-	RawAzPos  int16
-	RawElPos  int16
+	RawRegisters [12]uint16
+	Diag         uint16
+	RawAzPos     int16
+	RawElPos     int16
 	// AzPos and ElPos are in decimal degrees.
 	// They are calculated as 360*(reg/65536).
-	AzPos double
-	ElPos double
+	AzPos float64
+	ElPos float64
 	// AzVel and ElVel are in RPM.
 	// Positive indicates clockwise.
 	// They are calculated as 60*(reg/65536).
-	AzVel double
-	ElVel double
+	AzVel float64
+	ElVel float64
 	// Status contains the 48 status inputs.
 	Status [48]bool
 	// These are flags.
@@ -36,63 +38,91 @@ type Status struct {
 	ShutdownError   uint8
 }
 
-type Interface struct {
-	s *serial.Port
+func ParseRegisters(registers []uint16) Status {
+	var arr [12]uint16
+	for i, v := range registers {
+		if i >= len(arr) {
+			continue
+		}
+		arr[i] = v
+	}
+	status := Status{
+		RawRegisters: [12]uint16(arr),
+		Diag:         registers[0],
+		RawAzPos:     int16(registers[1]),
+		RawElPos:     int16(registers[2]),
+		AzPos:        360 * float64(registers[1]) / 65536,
+		ElPos:        360 * float64(int16(registers[2])) / 65536,
+		AzVel:        60 * float64(registers[3]) / 65536,
+		ElVel:        60 * float64(registers[4]) / 65536,
+	}
+	for i := range status.Status {
+		status.Status[i] = ((registers[5+(i/8)] >> (uint(i) % 8)) & 1) == 1
+	}
+	flags := registers[8]
+	status.LocalMode = flags&1 != 0
+	status.MaintenanceMode = flags&2 != 0
+	status.ElevationLower = flags&4 != 0
+	status.ElevationUpper = flags&8 != 0
+	status.Simulator = flags&16 != 0
+	status.BadCommand = flags&32 != 0
+	status.HostOkay = flags&64 != 0
+	status.ShutdownError = uint8(flags >> 10)
+	return status
 }
 
-func Connect(ctx context.Context, port string) (*Interface, error) {
+type StatusCallback func(status Status)
+
+type RCI struct {
+	s              *serial.Port
+	statusCallback StatusCallback
+}
+
+func Connect(ctx context.Context, port string, statusCallback StatusCallback) (*RCI, error) {
 	// Baud rate does not matter.
-	c := &serial.Config{Name: *serialPort, Baud: 9600}
+	c := &serial.Config{Name: port, Baud: 9600}
 	s, err := serial.OpenPort(c)
 	if err != nil {
 		return nil, err
 	}
-	i := &Interface{s}
-	go i.watch(ctx)
-	return i, nil
+	r := &RCI{s, statusCallback}
+	go r.watch(ctx)
+	return r, nil
 }
 
-func (i *Interface) watch(ctx context.Context) {
-	scanner := bufio.NewScanner(s)
+func (r *RCI) watch(ctx context.Context) {
+	// TODO: Close when ctx is canceled.
+	defer r.s.Close()
+	scanner := bufio.NewScanner(r.s)
 	for scanner.Scan() {
 		input := scanner.Text()
 		switch {
 		case input[0] == '!':
 			log.Printf(input)
-		default:
+		case len(input) > 0:
 			var registers []uint16
 			for _, word := range strings.Split(input, " ") {
-				i, err := strconv.ParseUint(input, 16, 16)
+				i, err := strconv.ParseUint(word, 16, 16)
 				if err != nil {
 					log.Printf("failed to parse %q: %v", input, err)
 				}
 				registers = append(registers, uint16(i))
 			}
-			status := Status{
-				RawRegisters: registers,
-				Diag:         registers[0],
-				RawAzPos:     int16(registers[1]),
-				RawElPos:     int16(registers[2]),
-				AzPos:        360 * double(registers[1]) / 65536,
-				ElPos:        360 * double(int16(registers[2])) / 65536,
-				AzVel:        60 * double(registers[3]) / 65536,
-				ElVel:        60 * double(registers[4]) / 65536,
-			}
-			for i := range status.Status {
-				status.Status[i] = ((registers[5+(i/8)] >> (i % 8)) & 1) == 1
-			}
-			flags := registers[8]
-			status.LocalMode = flags & 1
-			status.MaintenanceMode = flags & 2
-			status.ElevationLower = flags & 4
-			status.ElevationUpper = flags & 8
-			status.Simulator = flags & 16
-			status.BadCommand = flags & 32
-			status.HostOkay = flags & 64
-			status.ShutdownError = uint8(flags >> 10)
+			status := ParseRegisters(registers)
+			r.statusCallback(status)
 		}
 		if err := scanner.Err(); err != nil {
-			log.Errorf("reading serial port:", err)
+			log.Printf("reading serial port:", err)
 		}
+	}
+}
+
+func (r *RCI) Write(register int, values []uint16) {
+	out := []string{fmt.Sprintf("%x", register)}
+	for _, v := range values {
+		out = append(out, fmt.Sprintf("%x", v))
+	}
+	if _, err := r.s.Write([]byte("w" + strings.Join(out, " "))); err != nil {
+		log.Print(err)
 	}
 }
