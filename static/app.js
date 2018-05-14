904419d7841ee51a1b6f75bcee1c769fe65a0a52
Quentin Smith <quentin@mit.edu>
2018-05-14 00:19:27 -0400

    Fix click

--- static/app.js
+++ static/app.js
@@ -2,7 +2,6 @@ angular.module('app', ['components'])
     .controller('StatusController', function($scope, $locale, RCI) {
 	$scope.rci = RCI;
 	$scope.setAzimuthPosition = function($event) {
-	    if (!$event.buttons) { return; }
 	    let x = $event.offsetX-500;
 	    let y = $event.offsetY-500;
 	    let angle = Math.atan2(-y, x);
