b7e86addc5bcc0c1cf8083e6ca85f4d836b6b891
Quentin Smith <quentin@mit.edu>
2018-05-14 00:51:47 -0400

    Show elevation graphically as well

--- static/app.js
+++ static/app.js
@@ -1,7 +1,9 @@
 angular.module('app', ['components'])
     .controller('StatusController', function($scope, $locale, RCI) {
 	$scope.rci = RCI;
-	$scope.setAzimuthPosition = function($event) {
+    })
+    .controller('MapController', function($scope) {
+	$scope.mapClick = function($event) {
 	    let x = $event.offsetX-500;
 	    let y = $event.offsetY-500;
 	    let angle = Math.atan2(-y, x);
@@ -9,5 +11,12 @@ angular.module('app', ['components'])
 	    if (angle < 0) { angle += 2*Math.PI; }
 	    angle = -angle;
 	    $scope.rci.setAzimuthPosition(360*angle/(2*Math.PI));
+	};
+	$scope.elClick = function($event) {
+	    let x = $event.offsetX-10;
+	    let y = $event.offsetY-90;
+	    let angle = Math.atan2(y, x);
+	    angle = -angle;
+	    $scope.rci.setElevationPosition(360*angle/(2*Math.PI));
 	};
     });
