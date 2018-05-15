200cb26e92828951011f05e37796e570a86ffb2f
Quentin Smith <quentin@mit.edu>
2018-05-15 10:29:24 -0400

    Fix mouse position calculation in Firefox

--- static/app.js
+++ static/app.js
@@ -4,17 +4,21 @@ angular.module('app', ['components'])
     })
     .controller('MapController', function($scope) {
 	$scope.mapClick = function($event) {
-	    let x = $event.offsetX-500;
-	    let y = $event.offsetY-500;
+	    var rect = $event.currentTarget.ownerSVGElement.getBoundingClientRect();
+	    let x = ($event.clientX-rect.left)-500;
+	    let y = ($event.clientY-rect.top)-500;
 	    let angle = Math.atan2(-y, x);
 	    angle -= Math.PI/2;
 	    if (angle < 0) { angle += 2*Math.PI; }
 	    angle = -angle;
-	    $scope.rci.setAzimuthPosition(360*angle/(2*Math.PI));
+	    angle = 360*angle/(2*Math.PI);
+	    console.log($event.currentTarget, $event.offsetX, $event.offsetY, angle);
+	    $scope.rci.setAzimuthPosition(angle);
 	};
 	$scope.elClick = function($event) {
-	    let x = $event.offsetX-10;
-	    let y = $event.offsetY-90;
+	    var rect = $event.currentTarget.ownerSVGElement.getBoundingClientRect();
+	    let x = ($event.clientX-rect.left)-10;
+	    let y = ($event.clientY-rect.top)-90;
 	    let angle = Math.atan2(y, x);
 	    angle = -angle;
 	    $scope.rci.setElevationPosition(360*angle/(2*Math.PI));
