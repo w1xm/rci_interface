d2ad380ab00ccec5e65f16601e4f9a4ece8f4877
Quentin Smith <quentin@mit.edu>
2020-07-05 15:01:16 -0400

    Remote control for CPS20

--- w1xm_cps20/w1xm_cps20.ino
+++ w1xm_cps20/w1xm_cps20.ino
@@ -1,53 +1,96 @@
-/*
-	Example: DiscreteOutput
-
-	This example shows how to write to discrete output modules. 
-	Discrete ouputs are any output that is only ON or OFF such as a relay.
-
-	This example works with all P1000 Series:
-	 - Discrete ouput modules such as P1-08TRS, P1-08TD1, P1-16TR, etc.
-	 - Discrete combo modules such as P1-15CDD1, P1-15CDD2, P1-16CDR, etc.  
-
-	This example will toggle channel 2 of slot 1 on and off every second.
-	 _____  _____ 
-	|  P  ||  S  |
-	|  1  ||  L  |
-	|  A  ||  O  |
-	|  M  ||  T  |
-	|  -  ||     |
-	|  1  ||  0  |
-	|  0  ||  1  |
-	|  0  ||     |
-	 ¯¯¯¯¯  ¯¯¯¯¯ 
-	Written by FACTS Engineering
-	Copyright (c) 2019 FACTS Engineering, LLC
-	Licensed under the MIT license.
-*/
-
+// -*- mode: c++; -*-
 #include <P1AM.h>
+#include <Modbus.h>
+#include <ModbusSerial.h>
+
+#define MODBUS_SLAVE_ID 20
+#define MODBUS_BAUD 19200
+
+#define RELAYS 2
+
+// 5 seconds for time-delay relay, 3 seconds for spinup, plus safety margin
+#define DELAY_SECONDS 15
+
+// Output "coils":
+// Azimuth, Elevation
+// Input "discrete inputs":
+// ALL CONFIRM
+// AZ CONFIRM
+// EL CONFIRM
+// Input "input registers":
+// # of relays
+// "holding registers":
+// confirmation delay
+
+ModbusSerial mb;
 
 void setup(){ // the setup routine runs once:
-
-  Serial.begin(115200);  //initialize serial communication at 115200 bits per second 
+  pinMode(SWITCH_BUILTIN,INPUT);//Set our Switch (Pin 31) to be an input
   while (!P1.init()){ 
     ; //Wait for Modules to Sign on   
   }
-  pinMode(SWITCH_BUILTIN,INPUT);//Set our Switch (Pin 31) to be an input
+  mb.config(&Serial, MODBUS_BAUD, SERIAL_8N1, -1);
+  mb.setSlaveId(MODBUS_SLAVE_ID);
+
+  // Input register 0 contains the number of relays.
+  mb.addIreg(0, RELAYS);
+
+  // Holding register 0 contains the confirmation delay in seconds.
+  mb.addHreg(0, DELAY_SECONDS);
+
+  // Discrete input 0 shows all relays on.
+  mb.addIsts(0, false);
+
+  // Create one output coil and one discrete input for each relay.
+  for (int i = 0; i < RELAYS; i++) {
+    mb.addCoil(i, false); // Coil i = Power on
+    mb.addIsts(1+i, false); // Discrete input 1+i = Power confirm
+
+    // Set the output to off.
+    P1.writeDiscrete(false, 1, i+1);
+  }
 }
 
+// millis wrap every 50 days.
+unsigned long onMillis[RELAYS] = 0;
+
 void loop(){  // the loop routine runs over and over again forever:
-    bool switchState = digitalRead(SWITCH_BUILTIN);//Read the state of the switch
-    P1.writeDiscrete(switchState,1,1);
-    P1.writeDiscrete(switchState,1,2);
-    delay(100);
-//
-//  P1.writeDiscrete(LOW,1,1);
-//  P1.writeDiscrete(HIGH,1,2);  //Turn slot 1 channel 2 on
-//  Serial.print("2 on\n");
-//  delay(1000);  //wait 1 second
-//  P1.writeDiscrete(HIGH,1,1);
-//  P1.writeDiscrete(LOW,1,2);   //Turn slot 1 channel 2 off
-//  Serial.print("1 on\n");
-//  delay(1000);  //wait 1 second
-//  
+  bool switchState = digitalRead(SWITCH_BUILTIN);//Read the state of the switch
+
+  unsigned long confirmation_delay = mb.Hreg(0);
+
+  // Every time through the loop, we recompute the desired output
+  // state from the current values of the Modbus coils.
+
+  // Process any pending Modbus messages.
+  mb.task();
+
+  bool output[RELAYS];
+  for (int i = 0; i < RELAYS; i++) {
+    output[i] = mb.Coil(i) && switchState;
+    P1.writeDiscrete(output[i], 1, 1+i);
+    if (!output[i]) {
+      // Off -> immediately disable confirmation.
+      onMillis[i] = 0;
+      mb.Ists(1+i, false);
+    } else {
+      if (onMillis[i] == 0) {
+	onMillis[i] = millis();
+      }
+      if (!mb.Ists(1+i)) {
+	// Check if enough time has elapsed.
+	if (millis() > (onMillis[i]+confirmation_delay)) {
+	  mb.Ists(1+i, true);
+	} else if (millis() < onMillis[i]) {
+	  // Clock has wrapped, restart the timer.
+	  onMillis[i] = millis();
+	}
+      }
+    }
+  }
+  bool all_on = true;
+  for (int i = 0; i < RELAYS; i++) {
+    all_on &&= mb.Ists(1+i);
+  }
+  mb.Ists(0, all_on); // Expose on discrete input 0
 }
